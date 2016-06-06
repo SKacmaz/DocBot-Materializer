bb68e0e830a3978e20d3d7c18f5ba70b94b16f6f
Jochen Joswig <6joswig@informatik.uni-hamburg.de>
2016-06-06 17:41:03 +0200

    Model eines 2D-assoziativen Arrays als Grundlage für die Griddarstellung mit dem Roboter hinzugefügt

---
+++ src/model/GridPlane.java
@@ -0,0 +1,115 @@
+package model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class GridPlane {
+	private Map<String, Integer> users = new HashMap<String, Integer>();
+	private Map<String, Integer> types = new HashMap<String, Integer>();
+	
+	private int[][] grid;
+	
+	public GridPlane(){super();}
+	
+	public void addUser(String user){
+		this.users.put(user, new Integer(this.users.size()));
+	}
+	
+	public void addUsers(Set<String> users){
+		for(String user : users){
+			this.addUser(user);
+		}
+	}
+	
+	/**
+	 * Use this method to add a single type to the grid.
+	 * @param type
+	 */
+	public void addType(String type){
+		this.types.put(type, new Integer(this.types.size()));
+	}
+	
+	/**
+	 * Use this method to add multiple types to the grid.
+	 * @param types
+	 */
+	public void addTypes(Set<String> types){
+		for(String type : types){
+			this.addType(type);
+		}
+	}
+	
+	/**
+	 * Initializes the grid (how many rows and how many columns).
+	 * Only use this method after you have added users and types!
+	 * @see addUser(), addUsers(), addType(), addTypes()
+	 */
+	public void initialize(){
+		this.grid = new int[this.users.size()][this.types.size()];
+	}
+	
+	/**
+	 * Initializes the grid (how many rows and how many columns).
+	 * Use this method as a shortcut to adding users and types manually.
+	 * @param users
+	 * @param types
+	 */
+	public void initialize(Set<String> users, Set<String> types){
+		this.addUsers(users);
+		this.addTypes(types);
+		
+		this.grid = new int[this.users.size()][this.types.size()];
+	}
+	
+	/**
+	 * increases the resource-counter for a given user and type of resource
+	 * @param user
+	 * @param type
+	 */
+	public void increase(String user, String type){
+		int i = this.users.get(user).intValue();
+		int j = this.types.get(type).intValue();
+		
+		this.grid[i][j]++;
+		//TODO tell the Bot what to do
+	}
+	
+	/**
+	 * decreases the resources-counter for a given user and type of resource
+	 * @param user
+	 * @param type
+	 */
+	public void decrease(String user, String type){
+		int i = this.users.get(user).intValue();
+		int j = this.types.get(type).intValue();
+		
+		this.grid[i][j]--;
+		//TODO tell the Bot what to do
+	}
+
+	public Map<String, Integer> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Map<String, Integer> users) {
+		this.users = users;
+	}
+
+	public Map<String, Integer> getTypes() {
+		return types;
+	}
+
+	public void setTypes(Map<String, Integer> types) {
+		this.types = types;
+	}
+
+	public int[][] getGrid() {
+		return grid;
+	}
+
+	public void setGrid(int[][] grid) {
+		this.grid = grid;
+	}
+	
+}
