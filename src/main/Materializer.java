2a52c6f1308b13c82bce08fb4c1bd05b61e7d4c5
Kim Reichert <kimrdot@gmail.com>
2016-06-17 12:58:10 +0200

    created StartUp and Test classes

---
+++ src/main/Materializer.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Represents the controller of this Service. The Materializer receives update information from Haven
+ * and decides 
+ *
+ * @author Kim
+ */
+public class Materializer {
+
+}
